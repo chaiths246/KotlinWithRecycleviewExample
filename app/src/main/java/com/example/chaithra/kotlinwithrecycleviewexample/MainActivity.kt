bdc896db9e6e42bc8850fff8dcf62ccbce5001a3
chaiths246@gmail.com <chaiths246@gmail.com>
2019-11-23 12:49:52 -0800

    Added AddOnclick listner on action bar

--- app/src/main/java/com/example/chaithra/kotlinwithrecycleviewexample/MainActivity.kt
+++ app/src/main/java/com/example/chaithra/kotlinwithrecycleviewexample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.chaithra.kotlinwithrecycleviewexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_dialog.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,13 +31,15 @@ class MainActivity : AppCompatActivity() {
         rv_animal_list.adapter = DataAdpter(itmes, this)
 
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return true
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         R.id.add -> {
-
+            onAddClick()
             true
         }
         else -> super.onOptionsItemSelected(item)
@@ -52,4 +57,34 @@ class MainActivity : AppCompatActivity() {
 
 //
     }
+
+    fun onAddClick() {
+        //Inflate the dialog with custom view
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Add item")
+        //show dialog
+        val mAlertDialog = mBuilder.show()
+
+        mDialogView.addButton.setOnClickListener {
+
+            val item = mDialogView.dialogNameEt.text.toString()
+            itmes.add(item)
+
+            rv_animal_list.adapter!!.notifyDataSetChanged()
+
+            mAlertDialog.dismiss();
+
+        }
+        mDialogView.cancelBtn.setOnClickListener {
+
+
+
+            mAlertDialog.dismiss();
+
+        }
+    }
 }
+
