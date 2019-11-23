b5427623f2da7d1ab988a99d5e711455285ee05d
chaiths246@gmail.com <chaiths246@gmail.com>
2019-11-22 20:57:36 -0800

    Working adding dynamic to the Recycleview Using Kotlin

---
+++ app/src/main/java/com/example/chaithra/kotlinwithrecycleviewexample/DataAdpter.kt
@@ -0,0 +1,31 @@
+package com.example.chaithra.kotlinwithrecycleviewexample
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.data_list.view.*
+
+class DataAdpter(private val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<DataAdpter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.data_list, p0, false))
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+
+        p0?.items?.text = items.get(p1)
+    }
+
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val items = view.item
+    }
+}
---
+++ app/src/main/java/com/example/chaithra/kotlinwithrecycleviewexample/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.chaithra.kotlinwithrecycleviewexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    val itmes: ArrayList<String> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        additems()
+
+
+        rv_animal_list.layoutManager = LinearLayoutManager(this)
+
+
+        rv_animal_list!!.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        rv_animal_list.adapter = DataAdpter(itmes, this)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.add -> {
+
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+
+    fun additems() {
+        itmes.add("book")
+        itmes.add("pencil")
+        itmes.add("Pen")
+        itmes.add("Scale")
+        itmes.add("bag")
+        itmes.add("Lunchbox")
+        itmes.add("motorcyle")
+
+
+    }
+}
